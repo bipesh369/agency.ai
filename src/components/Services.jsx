4797fdcb01b115c9365cb3f5fc8bce6e5da8351e
bipesh369 <juniorbipesh@gmail.com>
2026-07-17 11:08:54 +0000

    add services component

---
+++ src/components/Services.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import assets from '../assets/assets'
+
+const Services = () => {
+
+  const servicesData = [
+    {
+      title: 'Advertising',
+      description: 'We turn bold ideas into powerfull digital solutions that connect, engage...',
+      icon: assets.ads_icon
+    },
+    {
+      title: 'Content Marketing',
+      description: 'We help you execute your plan and deliver results.',
+      icon: assets.marketing_icon
+    },
+    {
+      title: 'Content Writing',
+      description: 'We help you create a marketing strategy that drives results.',
+      icon: assets.content_icon
+    },
+    {
+      title: 'Social Media',
+      description: 'We help you build a strong social media presence and engage with your audience.',
+      icon: assets.social_icon
+    },
+  ]
+  return (
+    <div id='services' className='relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white' >
+
+      <img src={assets.bgImage2} alt="" className='absolute -top-110 -left-70-z-1 dark:hidden'/>
+
+    </div>
+  )
+}
+
+export default Services
