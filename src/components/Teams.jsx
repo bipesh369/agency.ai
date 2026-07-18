5ccbe381966a5d86b91f6fc3c53613a6224f2033
bipesh369 <juniorbipesh@gmail.com>
2026-07-18 18:51:44 +0000

    add contactus component

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -7,7 +7,7 @@ const Teams = () => {
     <div className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-800 dark:text-white'>
       <Title title='Meet the team' desc='A passionate team of digital experts dedicated to your brands success.'/>
 
-      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 w-full'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5'>
         {teamData.map((team, index)=>(
           <div key={index} className='flex max-sm:flex-col items-center gap-5 p-4 rounded-xl border border-gray-100 dark:border-gray-700 bg-white dark:bg-gray-900 shadow-xl shadow-gray-100 dark:shadow-white/5 hover:scale-105 transition-all duration-300'>
             <img src={team.image} alt="" className='w-12 h-12 rounded-full'/>
